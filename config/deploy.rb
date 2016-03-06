b0f52fc4da81f9c72c3d209be5a01950cacb252e
Jamie Tanna <jamie@jamietanna.co.uk>
2016-03-06 13:03:43 +0000

    Deploy from the Hacksoc Repo, not Jamie's

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'hacknotts'
-set :repo_url, 'git@github.com:jamietanna/hacknotts.com'
+set :repo_url, 'git@github.com:HackSocNotts/hacknotts.com.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
