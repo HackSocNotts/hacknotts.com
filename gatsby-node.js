4bcc1851a1f4c80f3f1a6fb3b89972a42ae9bdd5
Aaron Osher <git@aaronosher.io>
2020-10-09 02:47:13 +0100

    Add React-Bootstrap and make @DanielCordell's SCSS less miserable

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,15 +51,3 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     })
   })
 }
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
