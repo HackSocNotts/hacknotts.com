3da28b74b650c3ffe9309fb985ec911696e5d028
Daniel <danielbc@hotmail.co.uk>
2020-11-08 12:34:40 +0000

    Bruce

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,17 +1,22 @@
 import React,  { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 
-import bruceStanding from "../../static/bruce/brucestanding.png";
+import bruceHug from "../../static/bruce/bruce_hugwink.png";
+import bruceNeutral from "../../static/bruce/bruce_neutral.png";
+import bruceWave from "../../static/bruce/bruce_blushwave.png";
 
 export default function Intro(props) {
  return <Container>
     <div className="main-text">
     Every year HackNotts gathers hundreds of hackers to engage with new technologies, engage with each other, and engage with the tech industry. Learn new skills, meet new people, and enjoy workshops and prizes from some great tech companies from Nottingham and beyond.
     <br/>
-    <div style={{display: "flex", placeContent: "center"}}><img src={bruceStanding} className="bear"/></div>
     <br/>
     Leaving our usual venue of the Ada Lovelace Computing Lab at UoN HackNotts 2020 is coming to internet connected host near you! 
-    <br/><br/>
+    <div className="bear-wrapper" >
+        <img src={bruceHug} className="bear hideimage-2"/>
+        <img src={bruceNeutral} className="bear"/>
+        <img src={bruceWave} className="bear hideimage-1"/>
+    </div>
 Join us online this year for everything HackNotts is known for sans the food: great people, fascinating workshop content, unique swag, games corners, a <a href="https://twitter.com/BruceBernardII">giant bear</a>, and more.
     </div>
 </Container>;
--- src/components/partners.js
+++ src/components/partners.js
@@ -11,12 +11,13 @@ export default function Partners() {
   return <Container>
     <Row style={{marginTop: "32px", marginBottom: "32px", justifyContent: "center"}}>
       <Partner photo={HackathonsUK} link="https://hack.athon.uk/"/>
+
     </Row>
   </Container>;
 }
 
 function Partner(props) {
-  return <Col xs={4} style={{textAlign: "center"}}>
+  return <Col className="xs-6 md-4" style={{textAlign: "center"}}>
     <a href={props.link}><img style={{width: "15em", maxWidth: "25vw", borderRadius: "5px"}} src={props.photo}/></a>
   </Col>;
 }
