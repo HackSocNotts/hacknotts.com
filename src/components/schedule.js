3fd4331d40d3d687d4307caefad476264e5882c4
Daniel <danielbc@hotmail.co.uk>
2020-11-22 11:07:05 +0000

    Fixed padding

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -20,31 +20,35 @@ export default function Schedule(props) {
         </thead>
         <tbody class="main-text" style={{ textAlign: "center" }}>
           <tr class="intro-text">
-            <th colSpan="2">Saturday</th>
+            <th></th>
+            <th style={{padding: "1em"}}><i>Sunday</i></th>
           </tr>
           <ScheduleRow
-            time="11:00"
+            time="10:00"
             activity="Opening Ceremony/Hacking Starts"
           />
-          <ScheduleRow time="11:30" activity="Opening Ceremony Q&A" />
-          <ScheduleRow time="12:00" activity="Team Forming" />
-          <ScheduleRow time="12:30" activity="Capital One API Demo" />
-          <ScheduleRow time="13:00" activity="Workshop" />
-          <ScheduleRow time="13:45" activity="Workshop" />
-          <ScheduleRow time="15:00" activity="Workshop" />
-          <ScheduleRow time="16:15" activity="Workshop" />
-          <ScheduleRow time="17:30" activity="Talk" />
-          <ScheduleRow time="19:00" activity="Clash of Code Mini Event" />
-          <ScheduleRow time="20:00" activity="Pub Quiz Mini Event" />
+          <ScheduleRow time="10:30" activity="Opening Ceremony Q&A" />
+          <ScheduleRow time="11:00" activity="Team Forming" />
+          <ScheduleRow time="11:30" activity="Capital One API Demo" />
+          <ScheduleRow time="11:00" activity="Twilio Workshop" />
+          <ScheduleRow time="13:45" activity="Github Workshop" />
+          <ScheduleRow time="14:00" activity="Capital One Workshop" />
+          <ScheduleRow time="15:15" activity="Workshop" />
+          <ScheduleRow time="16:30" activity="Parsec Talk" />
+          <ScheduleRow time="17:30" activity="Clash of Code Mini Event" />
+          <ScheduleRow time="18:30" activity="Pub Quiz Mini Event" />
           <ScheduleRow
             time="Evening"
             activity="Game Night on Discord & Stream"
           />
           <tr class="intro-text">
-            <th colSpan="2">Sunday</th>
+            <th></th>
+            <th style={{padding: "1em"}}><i>Sunday</i></th>
           </tr>
           <ScheduleRow time="00:00" activity="Live RPG on Stream" />
+          <ScheduleRow time="01:00" activity="Workshops" />
           <ScheduleRow time="04:00" activity="Phasmaphobia on Stream" />
+          <ScheduleRow time="07:00" activity="Mock Interview Game Show" />
           <ScheduleRow time="08:00" activity="Cook-Off on Stream" />
           <ScheduleRow time="09:00" activity="Submission Help Desk" />
           <ScheduleRow time="09:30" activity="Submissions Due" />
