8e858d8fcf7327ae8fa2f8e2a9ca58106fbbdc63
dthelegend <me@daudi.dev>
2023-07-11 21:52:20 +0100

    Add some redirect scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr"
 import { resolve } from 'path'
 
 const appDir = resolve(__dirname, './src')
