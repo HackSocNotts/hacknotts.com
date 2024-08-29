2387489c4c5bafa38ae92e1321277defb2ac03e0
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-08-29 15:23:57 +0100

    Wipe HN84

--- src/views/index.ts
+++
@@ -1,7 +0,0 @@
-import Home from './home.tsx'
-import Sponsors from './sponsors.tsx'
-import Schedule from './schedule.tsx'
-import About from './about.tsx'
-import FAQ from './faq.tsx'
-
-export { Home, Sponsors, Schedule, About, FAQ }
