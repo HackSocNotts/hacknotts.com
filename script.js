3e8a334b50a5d8261b82b4e86c8d5eb51b513747
Alex Beeley <psyab30@nottingham.ac.uk>
2025-07-01 14:00:04 +0100

    add activities functionality

--- script.js
+++ script.js
@@ -3,22 +3,64 @@ const contentsMenu = document.querySelector(".contents-menu");
 
 const upButton = document.querySelector(".up-button");
 
+// ============================= ACTIVITY SECTION =============================
+const activityButtons = (document.querySelectorAll(".activity-sidebar-button"))
+const activityContent = document.querySelector(".activity-content");
+const activityContentHeader = activityContent.children[0];
+const activityContentText = activityContent.children[1];
+const activityTitles = [
+    "HNTV",
+    "GAMING LEAGUE",
+    "KARAOKE",
+    "CUP STACKING",
+    "WEREWOLF",
+    "CHESS",
+    "POKER"
+]
+const activityParagraphs = [
+    /* HNTV */ "HNTV is our 24hr stream that runs throughout the event, keeping you company as you work. We broadcast the entire event and cover a range of content, including midnight shitposting, Hacker interviews, game commentary and more! We also put important announcements up on the screens using the stream, enabling us to keep you updated throught the event!",
+    /* Gaming League */ "New this year, we are running a Gaming League! A tournament style competition where the winning team will earn themselves a tasty prize! Grab a ticket to join our Discord server and help us decide on a game! Current games being considered are Valorant, CS2, League etc.",
+    /* Karaoke */ "New this year, we're debuting a Karaoke room! Take some time off from your coding to sing along to your favourite tunes, open to all! The current plans for the room are to include mood lighting, mics and a projector screen for you to jam out.",
+    /* Cup Stacking */ "HackNott's cup stacking is an event held every year where you can show off your cup stacking skills. Want to compete to build the tallest tower or simply learn more about cup stacking? HackNotts is for you!",
+    /* Werewolf */ "Werewolf is held every HackNotts just after midnight, and is a game consisting of befuddlement and betrayal. Even if you haven't playe Werewolf before, you would have played other games similar like Among Us, Mafia or Town of Salem - feel free to try it out!",
+    /* Chess */ "Chess connoisseur? Complete beginner? Join our Chess tournament to test out your chess skills or improve your game! Consisting of multiple rounds with the first being on Chess.com and the following rounds being physical, make it to the top to win our Chess prize!",
+    /* Poker */ "New for 2024, Poker! Enjoy casual games (no real money) and defeat your fellow hackers to become HackNotts's best poker player!"
+]
+// ============================================================================
+
 contentsButton.addEventListener("click", showContents);
 upButton.addEventListener("click", goUp);
 
-var isShowing = false;
+for(let i = 0; i < activityButtons.length; i++)
+    activityButtons[i].addEventListener("click", () => selectActivity(i));
+
+var contentsIsShowing = false;
+
+var activityShowing = false;
 
 function showContents()
 {
-    if(!isShowing)
+    if(!contentsIsShowing)
         contentsMenu.classList.add("active");
     else
         contentsMenu.classList.remove("active");
 
-    isShowing = !isShowing;
+    contentsIsShowing = !contentsIsShowing;
 }
 
 function goUp()
 {
     scrollTo(0,0);
+}
+
+function selectActivity(buttonIndex)
+{
+    
+    console.log(buttonIndex)
+    activityContent.classList.remove("active");
+
+    activityContentHeader.innerHTML = activityTitles[buttonIndex];
+    activityContentText.innerHTML = activityParagraphs[buttonIndex];
+
+    activityContent.classList.add("active");
 }
