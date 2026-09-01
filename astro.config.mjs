5c16b0d77add8500661724ee869a139e2c60a02d
Sammy Sahnine <39201321+sammysahnine@users.noreply.github.com>
2026-09-01 19:31:46 +0100

    HackNotts '26 Loading Page (#158)
    * Draft for hn26
    
    * Added logo, changed css
    
    * Added further sponsor logos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
   integrations: [react()],
   site: 'https://hacknotts.com',
-  base: '',
 });
